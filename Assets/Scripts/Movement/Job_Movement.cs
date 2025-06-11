55d6d0c78b10af5b634a44a1f338091bcae1fa44
AnSeungHan <96778286+AnSeungHan@users.noreply.github.com>
2025-06-11 13:02:02 +0900

    Create Job_Movement.cs

---
+++ Assets/Scripts/Movement/Job_Movement.cs
@@ -0,0 +1,22 @@
+using Unity.Entities;
+using Unity.Burst;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+[BurstCompile]
+public partial struct Job_Movement : IJobEntity
+{
+    public float deltaTime;
+
+    public void Execute
+    (
+        in  MovementData            moveData,
+        ref LocalTransform          transform
+    )
+    {
+       if (!moveData.hasNewPosition)
+            return;
+
+       transform.Position = moveNextPosition;
+    }
+}
